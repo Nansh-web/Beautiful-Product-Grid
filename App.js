eaba19bf7fd0500c1cca5a9923b4b3c742049ed9
Nansh-web <97514741+Nansh-web@users.noreply.github.com>
2022-04-24 15:07:01 +0530

    Added HTML,CSS and all Pakages to create react app Sample

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import { Home, Main } from "./Home";
+// import styles from "./style.module.css";
+import ProductGrid from "./productgrid"
+
+function App() {
+  return (
+  <div>
+     <ProductGrid/>
+  </div>
+  )
+}
+
+export default App;
